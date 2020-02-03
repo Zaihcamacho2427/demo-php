08fa6de4fdd8704fe4616bc766a54446f872a2a1
Zaihcamacho2427 <42790672+Zaihcamacho2427@users.noreply.github.com>
2020-02-02 19:58:52 -0700

    Update Database.php

--- Database.php
+++ Database.php
@@ -5,7 +5,7 @@ class Database{
     private $dbservername = "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     private $dbusername = "tv8hy5xvcsed77v3";
     private $dbpassword = "h5f7psinzlm7xglc";
-    private $dbname = "php2-store";
+    private $dbname = "a97f0oqju8do3evk";
     
     function getConnection(){
         $connection = new mysqli($this->dbservername,$this->dbusername, $this->dbpassword, $this->dbname);
