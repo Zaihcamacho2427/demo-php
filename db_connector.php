e93e0a43ddf477debccf7b414e15058a887a6c7d
Zaihcamacho2427 <42790672+Zaihcamacho2427@users.noreply.github.com>
2020-02-02 19:59:17 -0700

    Update db_connector.php

--- db_connector.php
+++ db_connector.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 $servername = "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "tv8hy5xvcsed77v3";
 $password = "h5f7psinzlm7xglc";
-$dbname = "php2-store";
+$dbname = "a97f0oqju8do3evk";
 
 // Create connection
 $connection = mysqli_connect($servername, $username, $password, $dbname);
